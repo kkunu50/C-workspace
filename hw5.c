1bd53e2f8e6398103a7ae9d1f1bb4b4f1291e5c7
kkunu50 <kkunu0015@naver.com>
2021-11-04 14:45:03 +0900

    Add C File[hw5.c]

---
+++ hw5.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void) 
+{
+	int arr[5];
+	printf("5개의 정수를 입력하세요: ");
+	for (int i = 0; i <= 4; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	printf("홀수 출력: ");
+	for (int i = 0; i <= 4; i++) {
+		if (arr[i] % 2 == 1) {
+			printf("%d ", arr[i]);
+		}
+	}
+	printf("\n짝수 출력: ");
+	for (int i = 0; i <= 4; i++) {
+		if (arr[i] % 2 == 0) {
+			printf("%d ", arr[i]);
+		}
+	}
+	return 0;
+}
